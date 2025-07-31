b1327288f6b1d9a11d14b06a571a4889d20ab796
VictorAkashReznov <akashkushwaha5956@gmail.com>
2025-07-31 11:15:56 +0530

    2nd task is also completed

--- backend/index.js
+++ backend/index.js
@@ -3,23 +3,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 // Initialize express app
 const app = express();
+const apiRoute = require("./routes/api.route");
 
 app.use(express.json());
 app.use(cors());
 // Connect to MongoDB
-connectionString =
-  "mongodb+srv://mongo2024:mongo2024@cluster0.yvsdl.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce";
-mongoose
-  .connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/api", apiRoute);
 
 app.listen(3000, () => {
   console.log(`Server is running on port 3000`);
